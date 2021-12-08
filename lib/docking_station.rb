771ee95077a6a963c83c2bdb7b68f6ecf6ce4a6e
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-08 17:46:33 +0000

    stage 11 added dock_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -6,7 +6,21 @@ end
 
 
 class DockingStation
+  attr_reader :dock
+
+  def initialize
+    @dock = nil
+  end
+
   def release_bike
     Bike.new
   end
+
+  def dock_bike(bike)
+    @dock = bike
+    @dock
+  end
 end
+
+
+
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
 
     describe "#release_bike" do
       docking_station = DockingStation.new
-      it { is_expected.respond_to? (:release_bike) }
+      it { is_expected.to respond_to(:release_bike) }
     end
 
     describe "#release_bike" do
@@ -16,4 +16,13 @@ describe DockingStation do
         expect(subject.release_bike.working?).to eq(true)
       end
     end
+
+    describe "#dock_bike" do
+      # docking_station = DockingStation.new
+      it { is_expected.to respond_to(:dock_bike)}
+    end
+    
+      it "adds a Bike object into @dock" do
+        expect(subject.dock_bike(Bike)).to eq(Bike)
+      end
 end
