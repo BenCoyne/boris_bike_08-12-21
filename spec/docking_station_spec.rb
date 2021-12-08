2565d4081ae59afcca8542a8da5d997a66b5c720
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-08 12:31:56 +0000

    Refactored test to use 'one-liner' syntax

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,14 +1,11 @@
 require 'docking_station'
 
 describe DockingStation do
-
   docking_station = DockingStation.new
-
-  describe "#release_bike" do
-    it "responds to release_bike method" do
-      expect(docking_station).to respond_to(:release_bike)
+    describe "#release_bike" do
+      it { is_expected.respond_to? (:release_bike) }
     end
-  end
 end
+
 
 
