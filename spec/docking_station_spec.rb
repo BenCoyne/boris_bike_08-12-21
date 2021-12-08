acd1b03e40aab6eb4503a2f48933e54d8f4a2f51
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-08 16:26:28 +0000

    stage 10 complete - release_bike and working? methods

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,19 @@
 require 'docking_station'
 
 describe DockingStation do
-  docking_station = DockingStation.new
+
     describe "#release_bike" do
+      docking_station = DockingStation.new
       it { is_expected.respond_to? (:release_bike) }
     end
+
+    describe "#release_bike" do
+      it "releases a bike" do
+        expect(subject.release_bike).to be_a Bike
+      end
+
+      it "released bike, then check if bike works" do
+        expect(subject.release_bike.working?).to eq(true)
+      end
+    end
 end
-
-
-
