ae638268d2e26bfcfb281568bee651216c2697a8
Ben Coyne <bencoyne123@yahoo.co.uk>
2021-12-08 12:24:02 +0000

    Created lib folder with docking_station file (with code) and added further testing.

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,3 +1,14 @@
+require 'docking_station'
+
 describe DockingStation do
-  
+
+  docking_station = DockingStation.new
+
+  describe "#release_bike" do
+    it "responds to release_bike method" do
+      expect(docking_station).to respond_to(:release_bike)
+    end
+  end
 end
+
+
